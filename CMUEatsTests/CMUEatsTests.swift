c564428b580b0cbccc1acfa326be59a05ad307a4
Noah Goetz <noahgoetz@Noahs-MacBook-Pro.local>
2014-10-11 16:12:45 -0400

    Initial Commit

---
+++ CMUEatsTests/CMUEatsTests.swift
@@ -0,0 +1,36 @@
+//
+//  CMUEatsTests.swift
+//  CMUEatsTests
+//
+//  Created by Noah Goetz on 10/11/14.
+//  Copyright (c) 2014 Noah Goetz. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CMUEatsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
